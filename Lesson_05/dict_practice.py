569e3f5c74fc94333587194be958837b7e170964
Su Nguyen Tien <sunt@phenikaa-x.com>
2022-07-10 20:33:33 +0700

    Update code

---
+++ Lesson_05/dict_practice.py
@@ -0,0 +1,107 @@
+''' 1. From two lists, create a dictionary
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+'''
+
+'''2. Frome two dictionaries, merge into one
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+'''
+
+'''3. Print the value of key 'physics' from the below dict
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}Q
+'''
+
+'''4. Initialize dictionary with default values
+Given:
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+Expected output:
+{'Kelly': {'designation': 'Developer', 'salary': 8000}, 'Emma': {'designation': 'Developer', 'salary': 8000}}
+'''
+
+'''5. Create a dictionary by extracting the keys from a given dictionary
+Given:
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+Keys to extract
+keys = ["name", "salary"]
+
+Expected output:
+{'name': 'Kelly', 'salary': 8000}
+'''
+
+'''6. Delete a list of keys from a dictionary
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+Keys to remove
+keys = ["name", "salary"]
+
+Expected output:
+{'city': 'New york', 'age': 25}
+'''
+
+'''7. Check if value 200 exists in the following dictionary.
+Given:
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+
+Expected output:
+200 present in a dict
+'''
+
+'''8. Rename a key city to a location in the following dictionary
+Given:
+sample_dict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+
+Expected output:
+{'name': 'Kelly', 'age': 25, 'salary': 8000, 'location': 'New york'}
+'''
+
+'''9. Get the key of a minimum value from the following dictionary
+sample_dict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+'''
+
+'''10. Change Brad’s salary to 8500 in the following dictionary.
+Given:
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
+
+Expected output:
+{
+   'emp1': {'name': 'Jhon', 'salary': 7500},
+   'emp2': {'name': 'Emma', 'salary': 8000},
+   'emp3': {'name': 'Brad', 'salary': 8500}
+}
+'''
