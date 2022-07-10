d2606abac35cc7756bbb35eaa94f1ecfae6ef34c
Su Nguyen Tien <sunt@phenikaa-x.com>
2022-07-10 20:42:47 +0700

    Update lesson 5

--- Lesson_05/dict_practice.py
+++ Lesson_05/dict_practice.py
@@ -2,11 +2,34 @@
 keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 '''
+# way 1
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+res_dict = dict(zip(keys, values))
+print(res_dict)
+
+# Way 2
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+# empty dictionary
+res_dict = dict()
+
+for i in range(len(keys)):
+    res_dict.update({keys[i]: values[i]})
+print(res_dict)
 
 '''2. Frome two dictionaries, merge into one
 dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 '''
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = {**dict1, **dict2}
+print(dict3)
 
 '''3. Print the value of key 'physics' from the below dict
 sampleDict = {
@@ -19,8 +42,15 @@ sampleDict = {
             }
         }
     }
-}Q
+}
 '''
+# understand how to located the nested key
+# sampleDict['class'] = {'student': {'name': 'Mike', 'marks': {'physics': 70, 'history': 80}}}
+# sampleDict['class']['student'] = {'name': 'Mike', 'marks': {'physics': 70, 'history': 80}}
+# sampleDict['class']['student']['marks'] = {'physics': 70, 'history': 80}
+
+# solution
+print(sampleDict['class']['student']['marks']['history'])
 
 '''4. Initialize dictionary with default values
 Given:
@@ -30,6 +60,15 @@ defaults = {"designation": 'Developer', "salary": 8000}
 Expected output:
 {'Kelly': {'designation': 'Developer', 'salary': 8000}, 'Emma': {'designation': 'Developer', 'salary': 8000}}
 '''
+
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+res = dict.fromkeys(employees, defaults)
+print(res)
+
+# Individual data
+print(res["Kelly"])
 
 '''5. Create a dictionary by extracting the keys from a given dictionary
 Given:
@@ -45,6 +84,35 @@ keys = ["name", "salary"]
 Expected output:
 {'name': 'Kelly', 'salary': 8000}
 '''
+# way 1
+sampleDict = { 
+  "name": "Kelly",
+  "age":25, 
+  "salary": 8000, 
+  "city": "New york" }
+
+keys = ["name", "salary"]
+
+newDict = {k: sampleDict[k] for k in keys}
+print(newDict)
+
+# way 2
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# keys to extract
+keys = ["name", "salary"]
+
+# new dict
+res = dict()
+
+for k in keys:
+    # add current key with its va;ue from sample_dict
+    res.update({k: sample_dict[k]})
+print(res)
 
 '''6. Delete a list of keys from a dictionary
 sample_dict = {
@@ -61,6 +129,33 @@ Expected output:
 {'city': 'New york', 'age': 25}
 '''
 
+# way 1
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+for k in keys:
+    sample_dict.pop(k)
+print(sample_dict)
+
+# way 2
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+sample_dict = {k: sample_dict[k] for k in sample_dict.keys() - keys}
+print(sample_dict)
+
 '''7. Check if value 200 exists in the following dictionary.
 Given:
 sample_dict = {'a': 100, 'b': 200, 'c': 300}
@@ -68,6 +163,10 @@ sample_dict = {'a': 100, 'b': 200, 'c': 300}
 Expected output:
 200 present in a dict
 '''
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+if 200 in sample_dict.values():
+    print('200 present in a dict')
 
 '''8. Rename a key city to a location in the following dictionary
 Given:
@@ -82,6 +181,16 @@ Expected output:
 {'name': 'Kelly', 'age': 25, 'salary': 8000, 'location': 'New york'}
 '''
 
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+sample_dict['location'] = sample_dict.pop('city')
+print(sample_dict)
+
 '''9. Get the key of a minimum value from the following dictionary
 sample_dict = {
   'Physics': 82,
@@ -89,6 +198,13 @@ sample_dict = {
   'history': 75
 }
 '''
+
+sample_dict = {
+    'Physics': 82,
+    'Math': 65,
+    'history': 75
+}
+print(min(sample_dict, key=sample_dict.get))
 
 '''10. Change Brad’s salary to 8500 in the following dictionary.
 Given:
@@ -105,3 +221,12 @@ Expected output:
    'emp3': {'name': 'Brad', 'salary': 8500}
 }
 '''
+
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 6500}
+}
+
+sample_dict['emp3']['salary'] = 8500
+print(sample_dict)
